dc57675fa3eb0acc526fc28fa96caa7513c4bc2c
Sudharsan S <sudharsan5470436@gmail.com>
2026-06-21 18:11:22 +0530

    feat:added a new skills on skill.js

--- src/data/skills.js
+++ src/data/skills.js
@@ -2,9 +2,14 @@ export const skills = [
   "HTML",
   "CSS",
   "JavaScript",
+  "Tailwind",
   "React",
   "Node.js",
   "Express",
+  "MySQL",
   "MongoDB",
   "PostgreSQL",
-];
+  "Prisma",
+  "TypeORM",
+
+  ];
